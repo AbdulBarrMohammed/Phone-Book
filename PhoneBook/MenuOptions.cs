3b73bd82192e4a4d90429050129fb3eb5b74508a
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-04-30 13:27:43 -0400

    menu options enum

---
+++ PhoneBook/MenuOptions.cs
@@ -0,0 +1,12 @@
+namespace PhoneBook
+{
+    public enum MenuOptions
+    {
+        AddPhone,
+        DeletePhone,
+        UpdatePhone,
+        ViewPhone,
+        ViewAllPhone,
+        Quit
+    }
+}
